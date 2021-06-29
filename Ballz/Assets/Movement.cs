6395536fae4ebd8af4c33dcb5b55fe69680ed17e
AdrielGenao <adrielgenao1@gmail.com>
2021-06-28 23:53:45 -0400

    change

--- Ballz/Assets/Movement.cs
+++ Ballz/Assets/Movement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Movement : MonoBehaviour
 {
@@ -8,20 +10,31 @@ public class Movement : MonoBehaviour
     public Camera cam;
     public Transform playerPos;
     public Vector3 mouseDir;
+    public GameObject aim;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) //If mouse press
+        if(Input.GetMouseButton(0)) //If mouse press
         {
             mouse = cam.ScreenToWorldPoint(Input.mousePosition); //Get mouse postion
+            mouse= new Vector3(mouse.x,mouse.y,0f);
             mouseDir=mouse-playerPos.position;
             mouseDir.z=0.0f;
             mouseDir=mouseDir.normalized;
-            Shoot();
+            playerPos.up=mouse-playerPos.position;              
+            mouseDir.x=mouse.x+(playerPos.position.x-mouse.x)/2;
+            mouseDir.y=mouse.y+(playerPos.position.y-mouse.y)/2;
+            playerPos.Rotate(0f,0f,90f);
+            StartCoroutine(Waiting());
         }
     }
     void Shoot(){
         player.AddForce(mouseDir*force,ForceMode2D.Impulse);
+    }
+    IEnumerator Waiting(){
+        GameObject aiming=Instantiate(aim,mouseDir,playerPos.rotation);
+        yield return new WaitForSeconds(0.001f);
+        Destroy(aiming);
     }
 }
