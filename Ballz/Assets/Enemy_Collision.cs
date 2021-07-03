d169a2e86e5ad5ab2d4f5b9fbb783fb773af8ca5
AdrielGenao <adrielgenao1@gmail.com>
2021-07-03 15:50:54 +0200

    change

--- Ballz/Assets/Enemy_Collision.cs
+++ Ballz/Assets/Enemy_Collision.cs
@@ -7,13 +7,13 @@ public class Enemy_Collision : MonoBehaviour
     public GameObject target;
     public Collider2D col;
     public Rigidbody2D player;
-    public Transform player_pos;
     public float force=20f;
     public Movement movement;
+    public Transform target_pos;
     // Update is called once per frame
     void OnCollisionEnter2D(Collision2D collide){ //If collision
             if(collide.gameObject.name=="Player"){ //If collision is an obstacle on map or enemy
-                player.AddForce(player_pos.up*force,ForceMode2D.Impulse);
+                player.AddForce(target_pos.up*force,ForceMode2D.Impulse);
                 movement.shots+=1;
                 Destroy(target);
             }
--- Ballz/Assets/Movement.cs
+++ Ballz/Assets/Movement.cs
@@ -11,35 +11,43 @@ public class Movement : MonoBehaviour
     public Transform playerPos;
     public Vector3 mouseDir;
     public GameObject aim;
-    public int shots=0;
+    public int shots=1;
+    public float distance;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0)) //If mouse press
-        {
-            mouse = cam.ScreenToWorldPoint(Input.mousePosition); //Get mouse postion
-            mouse= new Vector3(mouse.x,mouse.y,0f);
-            mouseDir=mouse-playerPos.position;
-            mouseDir.z=0.0f;
-            mouseDir=mouseDir.normalized;
-            playerPos.up=mouse-playerPos.position;              
-            mouseDir.x=mouse.x+(playerPos.position.x-mouse.x)/2;
-            mouseDir.y=mouse.y+(playerPos.position.y-mouse.y)/2;
-            playerPos.Rotate(0f,0f,90f);
-            StartCoroutine(Waiting());
-            if (Input.GetMouseButton(0)==false){
-                shots=0;
+        if(shots==1){
+            if (Input.GetMouseButtonUp(0)){
+                playerPos.Rotate(0f,0f,-90f);
+                shots-=1;
                 Shoot();
+            }
+            if(Input.GetMouseButton(0)) //If mouse press
+            {
+                mouse = cam.ScreenToWorldPoint(Input.mousePosition); //Get mouse postion
+                mouse = new Vector3(mouse.x,mouse.y,0f);
+                playerPos.up=mouse-playerPos.position;              
+                mouseDir.x=mouse.x+(playerPos.position.x-mouse.x)/2;
+                mouseDir.y=mouse.y+(playerPos.position.y-mouse.y)/2;
+                playerPos.Rotate(0f,0f,90f);
+                distance=Vector3.Distance(playerPos.position,mouse);
+                if (distance>3){
+                    distance=3;
+                }
+                StartCoroutine(Waiting());
+            }
         }
-    }
     void Shoot(){
         player.AddForce(playerPos.up*force,ForceMode2D.Impulse);
-    }
+        }
     IEnumerator Waiting(){
         GameObject aiming=Instantiate(aim,mouseDir,playerPos.rotation);
+        Vector3 scale=aiming.transform.localScale;
+        scale.x=distance;
+        aiming.transform.localScale=scale;
         yield return new WaitForSeconds(0.001f);
         Destroy(aiming);
+        }
     }
-}
 }
